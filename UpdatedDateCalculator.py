805258fa48accd7860ca9ce33df0c04e67fdbf2d
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-12-02 13:41:42 -0600

    fix: sending params and naming correct

--- UpdatedDateCalculator.py
+++ UpdatedDateCalculator.py
@@ -1,40 +1,44 @@
 from datetime import date
 import math
 
+#ask user start date (and eventually a Y/N prompt if they would like to use the current day)
 yearStart = int(input("Start Year: "))
 monthStart = int(input("Start Month (numerical): "))
 dayStart = int(input("Start Day: "))
-
-class Christmas:
-  def __init__(self, year, month, day):
-      self.year = year
-      self.month = month
-      self.day = day
-      print("THERE ARE: ")
-  #calculate them by hardcoding xmas date
-  #substraction of the days
-  def calculateDays(self):
-    christmas_day = date(self.yearEnd, self.monthEnd, self.dayEnd)
-    days_for_xmas = (christmas_day - date.today()).days
-    return  days_for_xmas
-  #print days
-  def printDays(self):
-    d = self.calculateDays()
-    if d < 0:
-        d = int(math.fabs(d))
-        print(str(d) + " DAYS AGO!")
-    elif d > 0:
-        print(str(d) + " DAYS LEFT!")
-    else:
-        print(str(d) + ", TODAY!")
-
+print("-------------------------")
 yearEnd = int(input("End Year: "))
 monthEnd = int(input("End Month (numerical): "))
 dayEnd = int(input("End Day: "))
 
+class ChosenDay:
+  def __init__(self, yearStart, monthStart, dayStart, yearEnd, monthEnd, dayEnd):
+      self.yearStart = yearStart
+      self.monthStart = monthStart
+      self.dayStart = dayStart
+      self.yearEnd = yearEnd
+      self.monthEnd = monthEnd
+      self.dayEnd = dayEnd
+      print("-----------------------")
+  #substraction of the days
+  def calculateDays(self):
+    selected_day = date(self.yearEnd, self.monthEnd, self.dayEnd)
+    start_day = date(self.yearStart, self.monthStart, self.dayStart)
+    days_for_selected_day = (selected_day - start_day)
+    return  days_for_selected_day
+  #print days
+  def printDays(self):
+    d = self.calculateDays()
+    print(d)
+    # if d < 0:
+    #     d = int(math.fabs(d))
+    #     print(str(d) + " DAYS AGO!")
+    # elif d > 0:
+    #     print(str(d) + " DAYS LEFT!")
+    # else:
+    #     print("TODAY!")
+#check if month is over 12
 if monthEnd <= 0 or monthEnd > 12:
     print("Invalid Month")
 else:
-    xmasDays = Christmas(yearEnd, monthEnd, dayEnd)
+    xmasDays = ChosenDay(yearStart, monthStart, dayStart, yearEnd, monthEnd, dayEnd)
     xmasDays.printDays()
-    
